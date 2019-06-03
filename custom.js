8247d70f8acbfa5b5736a7753832749118fdd0bd
alexle512 <alexle512@gmail.com>
2019-06-03 17:05:25 -0500

    jquery practice

---
+++ custom.js
@@ -0,0 +1,23 @@
+"use strict"; 
+
+$( document ).ready( function () {
+
+    $("#AppendButton").click(function () {
+        $("#EpicDiv").append("<b>Append</b>")
+    })
+
+    $("#PrependButton").click(function () {
+        $("#EpicDiv").prepend( "<b>Prepend</b> " )
+    })
+
+    $("#AfterButton").click(function () {
+        $("#EpicDiv").after( "<b>After</b> " )
+
+    })
+
+    $("#BeforeButton").click(function () {
+        $("#EpicDiv").before( "<b>Befire</b> " )
+
+    })
+   
+});
